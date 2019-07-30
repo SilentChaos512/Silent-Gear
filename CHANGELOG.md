7517dd7a4e8a861c30887f10e4adbf1a5d8f9992
SilentChaos512 <sworkman256@gmail.com>
2019-07-30 18:45:42 -0500

    Fix color handlers crashing the game in some cases (#39)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [1.3.3]
 ### Fixed
 - Hammers destroying the NBT of certain blocks, like shulker boxes and soul urns [Silent's Gems #384]
+- Color handlers crashing the game in some cases [#39]
 
 ## [1.3.2] - 2019-07-28
 ### Added
--- src/main/java/net/silentchaos512/gear/init/ModItems.java
+++ src/main/java/net/silentchaos512/gear/init/ModItems.java
@@ -33,32 +33,38 @@ public final class ModItems {
     static final Map<String, BlockItem> blocksToRegister = new LinkedHashMap<>();
 
     public static BlueprintPackageItem blueprintPackage;
-    public static CustomTippedUpgrade customTippedUpgrade;
+    public static CustomTippedUpgrade customTippedUpgrade = new CustomTippedUpgrade();
 
     public static BlockNamedItem flaxseeds;
     public static Item netherBanana;
     public static Item pebble;
 
-    public static CoreSword sword;
-    public static CoreDagger dagger;
-    public static CoreKatana katana;
-    public static CoreMachete machete;
-    public static CoreSpear spear;
-    public static CorePickaxe pickaxe;
-    public static CoreShovel shovel;
-    public static CoreAxe axe;
-    public static CoreHammer hammer;
-    public static CoreExcavator excavator;
-    public static CoreMattock mattock;
-    public static CoreSickle sickle;
-    public static CoreBow bow;
-    public static CoreCrossbow crossbow;
-    public static CoreSlingshot slingshot;
+    public static CoreSword sword = new CoreSword();
+    public static CoreDagger dagger = new CoreDagger();
+    public static CoreKatana katana = new CoreKatana();
+    public static CoreMachete machete = new CoreMachete();
+    public static CoreSpear spear = new CoreSpear();
+    public static CorePickaxe pickaxe = new CorePickaxe();
+    public static CoreShovel shovel = new CoreShovel();
+    public static CoreAxe axe = new CoreAxe();
+    public static CoreHammer hammer = new CoreHammer();
+    public static CoreExcavator excavator = new CoreExcavator();
+    public static CoreMattock mattock = new CoreMattock();
+    public static CoreSickle sickle = new CoreSickle();
+    public static CoreBow bow = new CoreBow();
+    public static CoreCrossbow crossbow = new CoreCrossbow();
+    public static CoreSlingshot slingshot = new CoreSlingshot();
 
-    public static CoreArmor helmet;
-    public static CoreArmor chestplate;
-    public static CoreArmor leggings;
-    public static CoreArmor boots;
+    public static CoreArmor helmet = new CoreArmor(EquipmentSlotType.HEAD, "helmet");
+    public static CoreArmor chestplate = new CoreArmor(EquipmentSlotType.CHEST, "chestplate");
+    public static CoreArmor leggings = new CoreArmor(EquipmentSlotType.LEGS, "leggings");
+    public static CoreArmor boots = new CoreArmor(EquipmentSlotType.FEET, "boots");
+
+    static {
+        // Seems colors events can fire before items are initialized in some case?
+        // So we need to construct the items right now...
+        initializeGear();
+    }
 
     private ModItems() {}
 
@@ -80,7 +86,7 @@ public final class ModItems {
             register(item.getName(), item.asItem());
         }
 
-        customTippedUpgrade = register("custom_tipped_upgrade", new CustomTippedUpgrade());
+        register("custom_tipped_upgrade", customTippedUpgrade);
 
         flaxseeds = register("flaxseeds", new BlockNamedItem(ModBlocks.FLAX_PLANT.asBlock(), getBaseProperties()));
         netherBanana = register("nether_banana", new Item(getBaseProperties()
@@ -107,27 +113,6 @@ public final class ModItems {
 
     private static void initializeGear() {
         // Build gear maps now because blueprints need them
-        sword = new CoreSword();
-        dagger = new CoreDagger();
-        katana = new CoreKatana();
-        machete = new CoreMachete();
-        spear = new CoreSpear();
-        pickaxe = new CorePickaxe();
-        shovel = new CoreShovel();
-        axe = new CoreAxe();
-        hammer = new CoreHammer();
-        excavator = new CoreExcavator();
-        mattock = new CoreMattock();
-        sickle = new CoreSickle();
-        bow = new CoreBow();
-        crossbow = new CoreCrossbow();
-        slingshot = new CoreSlingshot();
-
-        helmet = new CoreArmor(EquipmentSlotType.HEAD, "helmet");
-        chestplate = new CoreArmor(EquipmentSlotType.CHEST, "chestplate");
-        leggings = new CoreArmor(EquipmentSlotType.LEGS, "leggings");
-        boots = new CoreArmor(EquipmentSlotType.FEET, "boots");
-
         toolClasses.put("sword", sword);
         toolClasses.put("dagger", dagger);
         toolClasses.put("katana", katana);
