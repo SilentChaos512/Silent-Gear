f997f85e891781569e9ff48e7a65f8a17a9ef563
SilentChaos512 <sworkman256@gmail.com>
2019-08-19 12:18:48 -0500

    Mine and Slash: Increase minimum tier to 1, set randoms salvagable

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,11 @@
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+### Changed
+- Mine and Slash random drops are now tier 1 minimum
+- Mine and Slash random drops should now be salvagable
+
 ## [1.3.8] - 2019-08-13
 ### Fixed
 - Mine and Slash compatibility race condition
--- src/main/java/net/silentchaos512/gear/compat/mineandslash/SGearConfigItem.java
+++ src/main/java/net/silentchaos512/gear/compat/mineandslash/SGearConfigItem.java
@@ -1,18 +1,32 @@
 package net.silentchaos512.gear.compat.mineandslash;
 
 import com.robertx22.mine_and_slash.config.compatible_items.ConfigItem;
+import com.robertx22.mine_and_slash.saveclasses.item_classes.GearItemData;
+import com.robertx22.mine_and_slash.uncommon.datasaving.Gear;
 import net.minecraft.item.ItemStack;
 import net.silentchaos512.gear.SilentGear;
 import net.silentchaos512.gear.parts.PartManager;
+import net.silentchaos512.gear.util.GearData;
 import net.silentchaos512.gear.util.GearGenerator;
 
 public class SGearConfigItem extends ConfigItem {
     @Override
     public ItemStack create(ItemStack stack, int level) {
         ItemStack result = super.create(stack, level);
-        final int r1 = SilentGear.random.nextInt(PartManager.getHighestMainPartTier() + 1);
-        final int r2 = SilentGear.random.nextInt(PartManager.getHighestMainPartTier() + 1);
-        final int tier = (r1 + r2) / 2;
-        return GearGenerator.randomizeParts(result, tier);
+
+        if (GearData.getConstructionParts(result).isEmpty()) {
+            // Set random drops as salvageable
+            GearItemData gearItemData = Gear.Load(result);
+            gearItemData.isSalvagable = true;
+            Gear.Save(result, gearItemData);
+
+            // And randomize parts, since this item has no construction data
+            final int r1 = 1 + SilentGear.random.nextInt(PartManager.getHighestMainPartTier());
+            final int r2 = 1 + SilentGear.random.nextInt(PartManager.getHighestMainPartTier());
+            final int tier = (r1 + r2) / 2;
+            return GearGenerator.randomizeParts(result, tier);
+        }
+
+        return result;
     }
 }
