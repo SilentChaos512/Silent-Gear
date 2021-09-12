60a5640e1b32a857011388983bbd520e4f9a47fd
SilentChaos512 <sworkman256@gmail.com>
2021-09-12 10:38:05 -0500

    Make stats recalculate when missing

--- src/main/java/net/silentchaos512/gear/IProxy.java
+++ src/main/java/net/silentchaos512/gear/IProxy.java
@@ -2,12 +2,16 @@ package net.silentchaos512.gear;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
 
 public interface IProxy {
     @Nullable
     PlayerEntity getClientPlayer();
+
+    @Nullable
+    World getClientLevel();
 
     @Nullable
     MinecraftServer getServer();
