9bd4f211ef5a70a076022563838923a23eb43a40
SilentChaos512 <sworkman256@gmail.com>
2022-01-20 19:29:59 -0600

    Fixes #482 (probably), crash when placing netherwood saplings

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,8 +3,9 @@
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Unreleased]
+## [2.8.6] - 2022-01-20
 ### Fixed
+- Possible crash when placing netherwood saplings (tag not bound error) [#482]
 - Moonwalker trait not working on curios [#458]
 
 ## [2.8.5] - 2022-01-18
--- src/main/java/net/silentchaos512/gear/block/NetherwoodSapling.java
+++ src/main/java/net/silentchaos512/gear/block/NetherwoodSapling.java
@@ -1,31 +1,12 @@
-/*
- * Silent Gear -- NetherwoodSapling
- * Copyright (C) 2018 SilentChaos512
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation version 3
- * of the License.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package net.silentchaos512.gear.block;
 
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.SaplingBlock;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.SaplingBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import net.silentchaos512.gear.block.trees.NetherwoodTree;
 import net.silentchaos512.gear.init.ModTags;
-
-import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
+import net.silentchaos512.lib.util.TagUtils;
 
 public class NetherwoodSapling extends SaplingBlock {
     public NetherwoodSapling(Properties properties) {
@@ -34,6 +15,6 @@ public class NetherwoodSapling extends SaplingBlock {
 
     @Override
     protected boolean mayPlaceOn(BlockState state, BlockGetter worldIn, BlockPos pos) {
-        return state.is(ModTags.Blocks.NETHERWOOD_SOIL);
+        return TagUtils.containsSafe(ModTags.Blocks.NETHERWOOD_SOIL, state);
     }
 }
