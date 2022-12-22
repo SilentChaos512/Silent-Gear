4176dcf0bd479e8256b86ed9b8b13c035522aa32
SilentChaos512 <sworkman256@gmail.com>
2022-12-22 16:16:36 -0600

    Try to fix DataResource crash with some mods

--- src/main/java/net/silentchaos512/gear/util/DataResource.java
+++ src/main/java/net/silentchaos512/gear/util/DataResource.java
@@ -1,6 +1,13 @@
 package net.silentchaos512.gear.util;
 
 import net.minecraft.resources.ResourceLocation;
+import net.silentchaos512.gear.SilentGear;
+import net.silentchaos512.gear.api.material.IMaterial;
+import net.silentchaos512.gear.api.part.IGearPart;
+import net.silentchaos512.gear.api.traits.ITrait;
+import net.silentchaos512.gear.gear.material.MaterialManager;
+import net.silentchaos512.gear.gear.part.PartManager;
+import net.silentchaos512.gear.gear.trait.TraitManager;
 
 import java.util.function.Function;
 
@@ -9,5 +16,29 @@ import java.util.function.Function;
 public class DataResource<T> extends net.silentchaos512.gear.api.util.DataResource<T> {
     public DataResource(ResourceLocation id, Function<ResourceLocation, T> getter) {
         super(id, getter);
+    }
+
+    public static net.silentchaos512.gear.api.util.DataResource<IMaterial> material(String modPath) {
+        return material(SilentGear.getId(modPath));
+    }
+
+    public static net.silentchaos512.gear.api.util.DataResource<IMaterial> material(ResourceLocation id) {
+        return new net.silentchaos512.gear.api.util.DataResource<>(id, MaterialManager::get);
+    }
+
+    public static net.silentchaos512.gear.api.util.DataResource<IGearPart> part(String modPath) {
+        return part(SilentGear.getId(modPath));
+    }
+
+    public static net.silentchaos512.gear.api.util.DataResource<IGearPart> part(ResourceLocation id) {
+        return new net.silentchaos512.gear.api.util.DataResource<>(id, PartManager::get);
+    }
+
+    public static net.silentchaos512.gear.api.util.DataResource<ITrait> trait(String modPath) {
+        return trait(SilentGear.getId(modPath));
+    }
+
+    public static net.silentchaos512.gear.api.util.DataResource<ITrait> trait(ResourceLocation id) {
+        return new net.silentchaos512.gear.api.util.DataResource<>(id, TraitManager::get);
     }
 }
