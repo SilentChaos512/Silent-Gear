6aabb748ec85b77b03ce09c3d2b91e3295c1b0e2
Electroely <30663570+Electroely@users.noreply.github.com>
2025-03-27 06:54:50 -0400

    Remove unused code

--- src/main/java/net/silentchaos512/gear/entity/projectile/GearArrowEntity.java
+++ src/main/java/net/silentchaos512/gear/entity/projectile/GearArrowEntity.java
@@ -94,29 +94,4 @@ public class GearArrowEntity extends AbstractArrow {
         super.shootFromRotation(shooter, x, y, z, velocity * speedMulti, accuracy > 0f ? inaccuracy / accuracy : inaccuracy);
     }
 	
-	/*
-    private ItemStack arrowStack = ItemStack.EMPTY;
-
-    public GearArrowEntity(EntityType<? extends Arrow> type, Level worldIn) {
-        super(type, worldIn);
-    }
-
-    public GearArrowEntity(Level worldIn, double x, double y, double z, ItemStack pickupItemStack, @Nullable ItemStack firedWeapon) {
-        super(worldIn, x, y, z, pickupItemStack, firedWeapon);
-    }
-
-    public GearArrowEntity(Level worldIn, LivingEntity shooter, ItemStack pickupItemStack, @Nullable ItemStack firedWeapon) {
-        super(worldIn, shooter, pickupItemStack, firedWeapon);
-    }
-
-    public void setArrowStack(ItemStack stack) {
-        this.arrowStack = stack.copyWithCount(1);
-    }
-
-    @Override
-    public void shootFromRotation(Entity shooter, float x, float y, float z, float velocity, float inaccuracy) {
-        float speedMulti = GearData.getProperties(arrowStack).getNumber(GearProperties.PROJECTILE_SPEED);
-        float accuracy = GearData.getProperties(arrowStack).getNumber(GearProperties.PROJECTILE_ACCURACY);
-        super.shootFromRotation(shooter, x, y, z, velocity * speedMulti, accuracy > 0f ? inaccuracy / accuracy : inaccuracy);
-    }*/
 }
