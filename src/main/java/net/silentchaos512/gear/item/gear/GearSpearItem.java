221e342c9abceb4628100ab10f76e8338eac9af5
SilentChaos512 <sworkman256@gmail.com>
2025-11-23 14:33:55 -0600

    Spears no longer make a sweeping attack

---
+++ src/main/java/net/silentchaos512/gear/item/gear/GearSpearItem.java
@@ -0,0 +1,130 @@
+package net.silentchaos512.gear.item.gear;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.component.ItemAttributeModifiers;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.silentchaos512.gear.api.item.GearType;
+import net.silentchaos512.gear.api.item.GearWeapon;
+import net.silentchaos512.gear.client.util.GearClientHelper;
+import net.silentchaos512.gear.util.GearData;
+import net.silentchaos512.gear.util.GearHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class GearSpearItem extends Item implements GearWeapon {
+    private final Supplier<GearType> gearType;
+
+    public GearSpearItem(Supplier<GearType> gearType) {
+        super(GearHelper.getBaseItemProperties());
+        this.gearType = gearType;
+    }
+
+    @Override
+    public GearType getGearType() {
+        return this.gearType.get();
+    }
+
+    @Override
+    public boolean canAttackBlock(BlockState state, Level level, BlockPos pos, Player player) {
+        return !player.isCreative();
+    }
+
+    //region Standard tool overrides
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext tooltipContext, List<Component> tooltip, TooltipFlag flagIn) {
+        GearClientHelper.addInformation(stack, tooltipContext, tooltip, flagIn);
+    }
+
+    @Override
+    public ItemAttributeModifiers getDefaultAttributeModifiers(ItemStack stack) {
+        var builder = ItemAttributeModifiers.builder();
+        GearHelper.addAttributeModifiers(stack, builder);
+        return builder.build();
+    }
+
+    @Override
+    public boolean isValidRepairItem(ItemStack toRepair, ItemStack repair) {
+        return GearHelper.getIsRepairable(toRepair, repair);
+    }
+
+    @Override
+    public int getEnchantmentValue(ItemStack stack) {
+        return GearHelper.getEnchantmentValue(stack);
+    }
+
+    @Override
+    public void setDamage(ItemStack stack, int damage) {
+        GearHelper.setDamage(stack, damage, super::setDamage);
+    }
+
+    @Override
+    public int getMaxDamage(ItemStack stack) {
+        return GearData.getProperties(stack).getNumberInt(getDurabilityStat());
+    }
+
+    @Override
+    public boolean isFoil(ItemStack stack) {
+        return GearClientHelper.hasEffect(stack);
+    }
+
+    @Override
+    public boolean hurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        return GearHelper.hurtEnemy(stack, target, attacker);
+    }
+
+    @Override
+    public void postHurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        GearHelper.postHurtEnemy(stack, target, attacker);
+    }
+
+    @Override
+    public boolean mineBlock(ItemStack stack, Level worldIn, BlockState state, BlockPos pos, LivingEntity entityLiving) {
+        return GearHelper.onBlockDestroyed(stack, worldIn, state, pos, entityLiving);
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, Level worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
+        GearHelper.inventoryTick(stack, worldIn, entityIn, itemSlot, isSelected);
+    }
+
+    @Override
+    public boolean shouldCauseReequipAnimation(ItemStack oldStack, ItemStack newStack, boolean slotChanged) {
+        return GearClientHelper.shouldCauseReequipAnimation(oldStack, newStack, slotChanged);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext context) {
+        return GearHelper.onItemUse(context);
+    }
+
+    @Override
+    public <T extends LivingEntity> int damageItem(ItemStack stack, int amount, @Nullable T entity, Consumer<Item> onBroken) {
+        return GearHelper.damageItem(stack, amount, entity, onBroken);
+    }
+
+    @Override
+    public int getBarWidth(ItemStack stack) {
+        return GearHelper.getBarWidth(stack);
+    }
+
+    @Override
+    public int getBarColor(ItemStack stack) {
+        return GearHelper.getBarColor(stack);
+    }
+
+    //endregion
+}
