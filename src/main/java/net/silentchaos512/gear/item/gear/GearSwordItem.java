cb9926f4a3215a2f6016e4b465d1029ce06251e4
SilentChaos512 <sworkman256@gmail.com>
2026-08-25 20:42:15 -0500

    Update changelog 4.2.3

--- src/main/java/net/silentchaos512/gear/item/gear/GearSwordItem.java
+++ src/main/java/net/silentchaos512/gear/item/gear/GearSwordItem.java
@@ -35,7 +35,6 @@ public class GearSwordItem extends BasicGearItem implements GearWeapon {
     public Tool createToolProperties(ItemStack gear, GearPropertiesData properties, HolderGetter<Block> blocks) {
         return new Tool(
                 List.of(
-//                        Tool.Rule.minesAndDrops(HolderSet.direct(Holder.direct(Blocks.COBWEB)), 15.0F),
                         Tool.Rule.minesAndDrops(blocks.getOrThrow(SgTags.Blocks.SWORD_HIGH_EFFICIENT), 15.0f),
                         Tool.Rule.overrideSpeed(blocks.getOrThrow(BlockTags.SWORD_INSTANTLY_MINES), Float.MAX_VALUE),
                         Tool.Rule.overrideSpeed(blocks.getOrThrow(BlockTags.SWORD_EFFICIENT), 1.5f)
